545d9dc371a265ec0b36046cc1937d5a47b39289
Ekaterine (Catherine) Mitagvaria <63928803+catherineisonline@users.noreply.github.com>
2022-09-01 19:56:48 +0400

    removed gh pages

--- src/components/errors/Error404.js
+++ src/components/errors/Error404.js
@@ -1,6 +1,7 @@
 import React from "react";
 export default class Error404 extends React.Component {
   render() {
+    document.title = "Error | Pizza Time";
     return (
       <h2 className="error-404">
         Oh, Now! Couldn't find the page you are looking for!
