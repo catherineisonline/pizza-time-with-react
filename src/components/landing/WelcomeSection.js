bf1eb6988f819547b87d5fb008d084990d0be0d5
Ekaterine (Catherine) Mitagvaria <63928803+catherineisonline@users.noreply.github.com>
2022-09-26 19:59:37 +0400

    updated section descriptions and changed styling

--- src/components/landing/WelcomeSection.js
+++ src/components/landing/WelcomeSection.js
@@ -10,11 +10,7 @@ export default class WelcomeSection extends React.Component {
             Welcome to <span>Pizza Time</span> restaurant
           </h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-            consequat nibh quam, sit amet luctus mauris convallis in. Mauris ac
-            erat mollis, eleifend tellus quis, faucibus eros. In aliquam orci
-            mi. Mauris varius turpis a lectus eleifend iaculis. Donec aliquam
-            orci eu justo porttitor, eget ultricies magna facilisis.{" "}
+        Founded in 1982 we bring pizza slice by slice to the next level! Unique branding and being in the pizza industry is the formula to out success. We do understand what people want and convert desires to unique experiences of taste. Freshness, orignality and quality is only the small part of out priorities. Affortable costs, located almost anywhere you go, amazing online operational system to order food in one click. Easy to navigate pre-order options and amazing options for corporate parties! At Pizaa Time we care about you because you are the one who makes us special!
           </p>
         </section>
         <img
