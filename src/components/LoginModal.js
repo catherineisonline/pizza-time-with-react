4a5e2d2785632c8b02a7e17b3d321dcbd1019451
Ekaterine (Catherine) Mitagvaria <63928803+catherineisonline@users.noreply.github.com>
2022-10-10 21:50:40 +0400

    added input validation for login form

--- src/components/LoginModal.js
+++ src/components/LoginModal.js
@@ -1,66 +1,100 @@
 import React from "react";
 import "./LoginModal.css";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
-export default class LoginModal extends React.Component {
-  HideModal() {
+export default function LoginModal() {
+  const [formValue, setFormValue] = useState({ email: '', password: '' });
+  const [formError, setFormError] = useState({})
+  const [submit, setSubmit] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormError(validateForm(formValue))
+    setSubmit(true);
+
+  }
+
+  const handleValidation = (e) => {
+    const { name, value } = e.target;
+    setFormValue({ ...formValue, [name]: value })
+  }
+
+  const validateForm = (value) => {
+    let errors = {}
+    const emailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+    if (!value.email) {
+      errors.email = "Please enter email"
+    }
+    else if (!emailRegex.test(value.email)) {
+      errors.email = "Please enter valid email"
+    }
+    if (!value.password) {
+      errors.password = "Please enter a valid password"
+    }
+
+    return errors;
+  }
+
+  function HideModal() {
     const hiddenModal = document.querySelector(".modal");
     hiddenModal.classList.remove("active-modal");
   }
-  RemoveMenu() {
+  function RemoveMenu() {
     const hiddenMenu = document.querySelector(".menu");
     hiddenMenu.classList.remove("active");
   }
 
-  render() {
-    return (
-      <article className="modal">
-        <section className="modal-main">
-          <button
-            className="close-modal-btn"
-            type="button"
-            onClick={() => {
-              this.HideModal();
-              this.RemoveMenu();
-            }}
-          >
-            X
-          </button>
-          <section className="modal-content">
-            <h2>Log in</h2>
-            <input type="email" placeholder="Email"></input>
-            <p className="modal-email-error"></p>
-            <input type="password" placeholder="Password"></input>
-            <p className="modal-password-error"></p>
+  return (
+    <article className="modal">
+      <section className="modal-main">
+        <button
+          className="close-modal-btn"
+          type="button"
+          onClick={() => {
+            HideModal();
+            RemoveMenu();
+          }}
+        >
+          X
+        </button>
+        <section className="modal-content">
+          <h2>Log in</h2>
+          <form className="modal-content" onSubmit={handleSubmit}>
+            <input onChange={handleValidation} value={formValue.email} name="email" type="text" placeholder="Email"></input>
+            <span className="login-input-err">{formError.email}</span>
+            <input onChange={handleValidation} value={formValue.password} name="password" type="password" placeholder="Password"></input>
+            <span className="login-input-err">{formError.password}</span>
             <section className="login-and-signup">
               <Link
                 to="/register"
                 className="modal-signup-btn"
                 onClick={() => {
-                  this.HideModal();
-                  this.RemoveMenu();
+                  HideModal();
+                  RemoveMenu();
                 }}
               >
                 Sign up
               </Link>
-              <button className="modal-login-btn">Log in</button>
+              <button type="submit" className="modal-login-btn">Log in</button>
+
             </section>
-            <section className="modal-extras">
-              <Link
-                to="/password-recovery"
-                className="modal-forgot-password"
-                onClick={() => {
-                  this.HideModal();
-                  this.RemoveMenu();
-                  this.ShowMenu();
-                }}
-              >
-                Forgot password?
-              </Link>
-            </section>
+          </form>
+          <section className="modal-extras">
+            <Link
+              to="/password-recovery"
+              className="modal-forgot-password"
+              onClick={() => {
+                HideModal();
+                RemoveMenu();
+                // ShowMenu();
+              }}
+            >
+              Forgot password?
+            </Link>
           </section>
         </section>
-      </article>
-    );
-  }
+      </section>
+    </article>
+  );
 }
