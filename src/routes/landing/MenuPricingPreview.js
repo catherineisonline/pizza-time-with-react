5052e8165b4df9223779362d9241c7afb1242ab4
Ekaterine (Catherine) Mitagvaria <63928803+catherineisonline@users.noreply.github.com>
2022-12-26 14:35:16 -0300

    reduced menu pricing preview code

--- src/routes/landing/MenuPricingPreview.js
+++ src/routes/landing/MenuPricingPreview.js
@@ -8,152 +8,92 @@ import MargheritaPizza from "../../assets/images/margherita-pizza.jpg";
 import BBQPizza from "../../assets/images/BBQ-chicken-pizza.jpg";
 import ResetLocation from "../../helpers/ResetLocation";
 
+
+const menuPricing = [
+  {
+    id: 1,
+    img: CheesePizza,
+    name: "Cheese Pizza",
+    price: 5.00,
+    currency: "$",
+    description: "Mozzarella is a classic cheese for pizza. However, feel free to change things up with additional toppings."
+  },
+  {
+    id: 2,
+    img: VeggiePizza,
+    name: "Veggie Pizza",
+    price: 9.00,
+    currency: "$",
+    description: "Crescent-roll dough, baked in a log, becomes the perch for a ranch-dressing spread and fresh, crunchy vegetables."
+  },
+  {
+    id: 3,
+    img: PepperoniPizza,
+    name: "Pepperoni Pizza",
+    price: 5.00,
+    currency: "$",
+    description: "Traditional pepperoni pizza is made with pizza sauce, mozzarella cheese, and pepperoni."
+  },
+  {
+    id: 4,
+    img: MeatPizza,
+    name: "Meat Pizza",
+    price: 10.00,
+    currency: "$",
+    description: "The meat toppings can vary, but often include cooked and crumbled Italian sausage or ground beef, bacon, pepperoni, and sometimes sliced ham."
+  },
+  {
+    id: 5,
+    img: MargheritaPizza,
+    name: "Margherita Pizza",
+    price: 8.00,
+    currency: "$",
+    description: "This margherita pizza recipe tastes like a woodfired pie from Italy! It stars a flavor-popping pizza sauce and perfectly chewy pizza crust."
+  },
+  {
+    id: 6,
+    img: BBQPizza,
+    name: "BBQ Chicken Pizza",
+    price: 10.00,
+    currency: "$",
+    description: "Spicy barbecue sauce, diced chicken, cilantro, peppers, and onion all covered with cheese and baked to bubbly goodness!"
+  }
+]
+
+
 export default class MenuPricingPreview extends React.Component {
   render() {
-
     return (
       <article className="section-5 flex-container ">
         <h2 className="txt-center pop-font txt-white">Menu Pricing</h2>
         <p className="section-description">
-          Vestibulum et viverra massa, a malesuada lacus. Curabitur quis elit
-          eget metus porttitor sollicitudin at in nulla. Integer mollis orci vel
-          consequat eleifend. Fusce sollicitudin feugiat magna et facilisis.
+        Every bite of every meal is different and special. You will never get bored and you will always be able to find something new and interesting for you. Discover every mouthwatering option we have to offer
         </p>
         <section className="pricing-grid flex-container flex-column">
-          {/* Pizza 1 */}
-          <Link
-            onClick={ResetLocation}
-            to="/menu"
-            className="pricing-grid-item flex-container flex-row"
-          >
-            <img className="pricing-img" src={CheesePizza} alt="Cheese Pizza" />
-            <div className="pricing-details flex-container flex-column">
-              <div className="name-and-price flex-container flex-row txt-center">
-                <h3 className="pop-font">Cheese Pizza</h3>
-                <p>
-                  <span>$</span>5.00
-                </p>
-              </div>
-              <p>
-                Mozzarella is a classic cheese for pizza. However, feel free to
-                change things up with additional toppings.
-              </p>
-            </div>
-          </Link>
-          {/* Pizza 2 */}
-          <Link
-            onClick={ResetLocation}
-            to="/menu"
-            className="pricing-grid-item flex-container flex-row"
-          >
-            <img className="pricing-img" src={VeggiePizza} alt="eggie Pizza" />
-            <div className="pricing-details flex-container flex-column">
-              <div className="name-and-price flex-container flex-row txt-center">
-                <h3 className="pop-font">Veggie Pizza</h3>
-                <p>
-                  <span>$</span>9.00
-                </p>
-              </div>
-              <p>
-                Crescent-roll dough, baked in a log, becomes the perch for a
-                ranch-dressing spread and fresh, crunchy vegetables.
-              </p>
-            </div>
-          </Link>
-          {/* Pizza 3 */}
-          <Link
+        {menuPricing.map(menu => <Link
+        key={menu.id}
             onClick={ResetLocation}
             to="/menu"
             className="pricing-grid-item flex-container flex-row"
           >
             <img
               className="pricing-img"
-              src={PepperoniPizza}
-              alt="Pepperoni Pizza"
+              src={menu.img}
+              alt={menu.name}
             />
-            <div className="pricing-details flex-container flex-column">
-              <div className="name-and-price flex-container flex-row txt-center">
-                <h3 className="pop-font"> Pepperoni Pizza</h3>
+            <section className="pricing-details flex-container flex-column">
+              <section className="name-and-price flex-container flex-row txt-center">
+                <h3 className="pop-font">{menu.name}</h3>
                 <p>
-                  <span>$</span>5.00
+                  <span>{menu.currency}</span>{menu.price}
                 </p>
-              </div>
+              </section>
               <p>
-                Traditional pepperoni pizza is made with pizza sauce, mozzarella
-                cheese, and pepperoni.
+              {menu.description}
               </p>
-            </div>
+            </section>
           </Link>
-          {/* Pizza 4 */}
-          <Link
-            onClick={ResetLocation}
-            to="/menu"
-            className="pricing-grid-item flex-container flex-row"
-          >
-            <img className="pricing-img" src={MeatPizza} alt="Meat Pizza" />
-            <div className="pricing-details flex-container flex-column">
-              <div className="name-and-price flex-container flex-row txt-center">
-                <h3 className="pop-font">Meat Pizza</h3>
-                <p>
-                  <span>$</span>10.00
-                </p>
-              </div>
-              <p>
-                The meat toppings can vary, but often include cooked and
-                crumbled Italian sausage or ground beef, bacon, pepperoni, and
-                sometimes sliced ham.
-              </p>
-            </div>
-          </Link>
-          {/* Pizza 5 */}
-          <Link
-            onClick={ResetLocation}
-            to="/menu"
-            className="pricing-grid-item flex-container flex-row"
-          >
-            <img
-              className="pricing-img"
-              src={MargheritaPizza}
-              alt="Margherita Pizza"
-            />
-            <div className="pricing-details flex-container flex-column">
-              <div className="name-and-price flex-container flex-row txt-center">
-                <h3 className="pop-font">Margherita Pizza</h3>
-                <p>
-                  <span>$</span>8.00
-                </p>
-              </div>
-              <p>
-                This margherita pizza recipe tastes like a woodfired pie from
-                Italy! It stars a flavor-popping pizza sauce and perfectly chewy
-                pizza crust.
-              </p>
-            </div>
-          </Link>
-          {/* Pizza 6 */}
-          <Link
-            onClick={ResetLocation}
-            to="/menu"
-            className="pricing-grid-item flex-container flex-row"
-          >
-            <img
-              className="pricing-img"
-              src={BBQPizza}
-              alt="BBQ Chicken Pizza"
-            />
-            <div className="pricing-details flex-container flex-column">
-              <div className="name-and-price flex-container flex-row txt-center">
-                <h3 className="pop-font">BBQ Chicken Pizza</h3>
-                <p>
-                  <span>$</span>10.00
-                </p>
-              </div>
-              <p>
-                Spicy barbecue sauce, diced chicken, cilantro, peppers, and
-                onion all covered with cheese and baked to bubbly goodness!{" "}
-              </p>
-            </div>
-          </Link>
+          )}
         </section>
       </article>
     );
