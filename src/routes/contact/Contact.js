519f60015a1369d295c0918796e8a36e4b2afcc5
Ekaterine (Catherine) Mitagvaria <63928803+catherineisonline@users.noreply.github.com>
2023-02-28 19:05:28 -0300

    updated contact page

--- src/routes/contact/Contact.js
+++ src/routes/contact/Contact.js
@@ -37,70 +37,64 @@ const Contact = () => {
   }
   return (
     <motion.div
-    initial={{ opacity: 0, translateX: -300  }}
-    whileInView={{ opacity: 1 , translateX: 0  }}
-      exit={{ opacity: 0,  translateX: -300  }}
-      transition={{ duration: 1}}
+      initial={{ opacity: 0, translateX: -300 }}
+      whileInView={{ opacity: 1, translateX: 0 }}
+      exit={{ opacity: 0, translateX: -300 }}
+      transition={{ duration: 1 }}
     >
-    <main className="contact" >
-      <section>
-        {submit && Object.keys(formError).length === 0 ?
-          <p className="contact-success">We have recieved your message and we will get back to you shortly! 🍕</p> :
-          <form onSubmit={handleSubmit} className="flex-container flex-column">
-            <div className="webflow-style-input">
-              <input
-                onChange={handleValidation}
-                value={formValue.fullname}
-                name="fullname"
-                className="pop-font"
-                type="text"
-                placeholder="Full Name"
-              />
-            </div>
-            <span className="input-validation-error">{formError.fullname}</span>
-            <div className=" webflow-style-input">
-              <input
-                onChange={handleValidation}
-                value={formValue.email}
-                name="email"
-                className="pop-font"
-                type="text"
-                placeholder="Email"
-              />
-            </div>
-            <span className="input-validation-error">{formError.email}</span>
-            <div className=" webflow-style-input">
-              <textarea
-                onChange={handleValidation}
-                value={formValue.message}
-                name="message"
-                className="pop-font"
-                placeholder="Message"
-              />
-            </div>
-            <span className="input-validation-error">{formError.message}</span>
-            <button type="submit" className="active-button-style">
-              Send
-            </button>
-          </form>}
-      </section>
-      <section className="contact-us-img"></section>
-      <section className="contact-us-content pop-font">
-        <section className="contact-us-content-txt">
-          <h1 className="">Contact Us</h1>
-          <h3>
-            We are looking forward to hearing from you! Do not hesitate to
-            contact us.
-          </h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
-            eleifend magna sit amet sodales scelerisque. Morbi consectetur
-            lacus nibh, sed luctus mauris hendrerit quis. Nunc at efficitur
-            est.
-          </p>
+      <main className="contact" >
+        <section>
+          {submit && Object.keys(formError).length === 0 ?
+            <p className="contact-success">We have recieved your message and we will get back to you shortly! 🍕</p> :
+            <form onSubmit={handleSubmit} className="flex-container flex-column">
+              <div className="webflow-style-input">
+                <input
+                  onChange={handleValidation}
+                  value={formValue.fullname}
+                  name="fullname"
+                  className="pop-font"
+                  type="text"
+                  placeholder="Full Name"
+                />
+              </div>
+              <span className="input-validation-error">{formError.fullname}</span>
+              <div className=" webflow-style-input">
+                <input
+                  onChange={handleValidation}
+                  value={formValue.email}
+                  name="email"
+                  className="pop-font"
+                  type="text"
+                  placeholder="Email"
+                />
+              </div>
+              <span className="input-validation-error">{formError.email}</span>
+              <div className=" webflow-style-input">
+                <textarea
+                  onChange={handleValidation}
+                  value={formValue.message}
+                  name="message"
+                  className="pop-font"
+                  placeholder="Message"
+                />
+              </div>
+              <span className="input-validation-error">{formError.message}</span>
+              <button type="submit" className="active-button-style">
+                Send
+              </button>
+            </form>}
         </section>
-      </section>
-    </main>
+        <section className="contact-us-img"></section>
+        <section className="contact-us-content pop-font">
+          <section className="contact-us-content-txt">
+            <h1 className="">Contact Us</h1>
+            <p>
+              We are looking forward to hearing from you! Do not hesitate to
+              contact us in case you have questions.
+            </p>
+          </section>
+        </section>
+      </main>
     </motion.div>
   );
 }
