8939509f290495af33f4f72fe02574ac573d1d3f
Ekaterine (Catherine) Mitagvaria <63928803+catherineisonline@users.noreply.github.com>
2023-08-24 13:21:33 +0400

    added minor style improvement to login modal and contact page

--- src/routes/contact/Contact.js
+++ src/routes/contact/Contact.js
@@ -71,10 +71,9 @@ const Contact = () => {
         <section className="contact-us-img"></section>
         <section className="contact-us-content pop-font">
           <section className="contact-us-content-txt">
-            <h2 className="">Contact Us</h2>
+            <h2 className="">Contact us</h2>
             <p>
-              We are looking forward to hearing from you! Do not hesitate to
-              contact us in case you have questions.
+              We greatly anticipate your response and are eager to receive any inquiries you might have. Please do not hesitate to reach out to us should you require any further clarification or assistance. Your feedback and questions are of utmost importance to us, and we are here to provide the support you need. Looking forward to hearing from you!
             </p>
           </section>
         </section>
