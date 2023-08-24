b4f187998d296e22895099dbac1ecee1bed9dc98
Ekaterine (Catherine) Mitagvaria <63928803+catherineisonline@users.noreply.github.com>
2023-08-24 13:30:19 +0400

    added minor style improvement to about us page

--- src/routes/about/AboutSectionOne.js
+++ src/routes/about/AboutSectionOne.js
@@ -11,7 +11,7 @@ export default class AboutSectionOne extends React.Component {
       <article className="section-one">
         <section className="section-one-content">
           <h2>About us</h2>
-          <h3>More than delicious food</h3>
+          <h3 className='sub-title'>More than delicious food</h3>
           <p>
             Founded in 1982 we bring pizza slice by slice to the next level! Unique branding and being in the pizza industry is the formula to out success. We do understand what people want and convert desires to unique experiences of taste. Freshness, orignality and quality is only the small part of out priorities. Affortable costs, located almost anywhere you go, amazing online operational system to order food in one click. Easy to navigate pre-order options and amazing options for corporate parties! At Pizaa Time we care about you because you are the one who makes us special!
           </p>
