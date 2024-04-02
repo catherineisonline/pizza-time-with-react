1e4db780a22f085f6cce27cc1cafe4c6b06f6682
Ekaterine Mitagvaria <ekaterine@Ekaterines-MacBook-Air.local>
2024-04-02 15:41:13 +0400

    Initilize Turso database

--- backend/config/sql.mjs
+++ backend/config/sql.mjs
@@ -1,11 +1,21 @@
 import mysql from 'mysql2';
 import dotenv from 'dotenv';
+import { createClient } from "@libsql/client";
 dotenv.config();
 
-const database = process.env.REACT_APP_DATABASE_URL;
+// const database = process.env.REACT_APP_DATABASE_URL;
 
-const connection = mysql.createConnection(database);
+// const connection = mysql.createConnection(database);
+// export default connection.promise();
 
 
-export default connection.promise();
+const databaseURL = process.env.REACT_APP_TURSO_DB_URL;
+const authToken = process.env.REACT_APP_TURSO_DB_TOKEN;
 
+const client = createClient({
+    url: databaseURL,
+    authToken: authToken
+});
+
+
+export default client;
