c6743f01713e266ed178ee5420131d3eb4b9fcb3
catherineisonline <ekaterine.jpg@gmail.com>
2025-02-28 15:57:13 +0530

    refactor to semantic html and fix css issues

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,92 +1,111 @@
-import React from 'react'
-import logo from '../../assets/images/logo.png'
-import openMenu from '../../assets/images/open-menu.svg'
-import closeMenu from '../../assets/images/close-menu.svg'
-import { Link, NavLink } from 'react-router-dom'
-import Cart from '../../assets/images/cart-icon-dark.png'
-import SuccessMsg from '../SuccessMsg'
-import ResetLocation from '../../helpers/ResetLocation'
-import './header.css'
+import "./header.css";
+import React from "react";
+import logo from "../../assets/images/logo.png";
+import openMenu from "../../assets/images/open-menu.svg";
+import closeMenu from "../../assets/images/close-menu.svg";
+import { Link, NavLink } from "react-router-dom";
+import Cart from "../../assets/images/cart-icon-dark.png";
+import SuccessMsg from "../SuccessMsg";
+import ResetLocation from "../../helpers/ResetLocation";
+
 const headerMenu = [
   {
     to: "/",
-    label: "Home"
+    label: "Home",
   },
   {
     to: "/menu",
-    label: "Menu"
+    label: "Menu",
   },
   {
     to: "/blog",
-    label: "Blog"
+    label: "Blog",
   },
   {
     to: "/about",
-    label: "About"
+    label: "About",
   },
   {
     to: "/contact",
-    label: "Contact"
-  }
-]
-const Header = ({ loginModal,
+    label: "Contact",
+  },
+];
+const Header = ({
+  loginModal,
   productsQuantity,
   handleLogout,
   showModal,
   isModalActive,
   hideMenu,
-  validLogin, activateLoginModal }) => {
+  validLogin,
+  activateLoginModal,
+}) => {
   return (
-    <header aria-labelledby='title'>
+    <header aria-labelledby="title">
       {loginModal}
-      <nav className="header__nav flex-container flex-row txt-center" aria-label='Header Menu'>
+      <nav
+        className="header__nav flex-container flex-row txt-center"
+        aria-label="Header Menu">
         <NavLink
           onClick={() => {
-            ResetLocation()
-            hideMenu()
+            ResetLocation();
+            hideMenu();
           }}
           to="/"
-          className="logo-styling flex-container flex-row txt-center txt-white"
-        >
+          className="logo-styling flex-container flex-row txt-center txt-white">
           <img
             width="100"
             height="100"
             className="logo"
             src={logo}
-            alt="Pizza Time logo"
+            alt=""
+            aria-hidden="true"
           />
-          <h1 id='title' translate="no">
+          <h1
+            id="title"
+            translate="no">
             Pizza <span>Time</span>
           </h1>
         </NavLink>
-        <ul className={`header__nav__menu flex-row pop-font ${isModalActive ? 'active' : ''}`}>
-          {headerMenu.map(({ to, label }) =>
+        <ul
+          id="main-menu"
+          className={`header__nav__menu flex-row pop-font ${
+            isModalActive ? "active" : ""
+          }`}>
+          {headerMenu.map(({ to, label }) => (
             <li key={to}>
               <NavLink
                 onClick={() => {
-                  ResetLocation()
-                  hideMenu()
+                  ResetLocation();
+                  hideMenu();
                 }}
-                className={({ isActive }) => `txt-white ${isActive ? "header-active-link" : ""}`}
+                className={({ isActive }) =>
+                  `txt-white ${
+                    isActive && label !== "Home" ? "header-active-link" : ""
+                  }`
+                }
                 aria-current={({ isActive }) => (isActive ? "page" : undefined)}
-                to={to}
-              >
+                to={to}>
                 {label}
               </NavLink>
-            </li>)}
-          {validLogin && <li>
-            <NavLink
-              onClick={() => {
-                ResetLocation()
-                hideMenu()
-              }}
-              className={({ isActive }) => `txt-white ${isActive ? "header-active-link" : ""}`}
-              aria-current={({ isActive }) => (isActive ? "page" : undefined)}
-              to="/profile"
-            >
-              Profile
-            </NavLink>
-          </li>}
+            </li>
+          ))}
+          {validLogin && (
+            <li>
+              <NavLink
+                onClick={() => {
+                  ResetLocation();
+                  hideMenu();
+                }}
+                className={({ isActive }) =>
+                  `txt-white ${isActive ? "header-active-link" : ""}`
+                }
+                aria-current={({ isActive }) => (isActive ? "page" : undefined)}
+                to="/profile">
+                Profile
+              </NavLink>
+            </li>
+          )}
           <li>
             <div className="login-and-cart">
               {validLogin ? (
@@ -94,20 +113,18 @@ const Header = ({ loginModal,
                   to="/"
                   className="passive-button-style txt-white"
                   onClick={() => {
-                    ResetLocation()
-                    handleLogout()
-                  }}
-                >
+                    ResetLocation();
+                    handleLogout();
+                  }}>
                   Log out
                 </Link>
               ) : (
                 <button
                   className="passive-button-style txt-white"
                   onClick={() => {
-                    ResetLocation()
-                    activateLoginModal()
-                  }}
-                >
+                    ResetLocation();
+                    activateLoginModal();
+                  }}>
                   Log in
                 </button>
               )}
@@ -115,11 +132,14 @@ const Header = ({ loginModal,
                 className="cart-btn active-button-style txt-white"
                 to="/cart"
                 onClick={() => {
-                  ResetLocation()
-                  hideMenu()
-                }}
-              >
-                <img src={Cart} alt="Shopping Cart" />
+                  ResetLocation();
+                  hideMenu();
+                }}>
+                <img
+                  src={Cart}
+                  alt=""
+                  aria-hidden="true"
+                />
                 <p>Cart</p>
                 <p>({productsQuantity})</p>
               </NavLink>
@@ -131,16 +151,18 @@ const Header = ({ loginModal,
           aria-label={isModalActive ? "Close menu" : "Open menu"}
           aria-expanded={isModalActive}
           aria-controls="main-menu"
-          onClick={showModal}
-        >
-          <img width="80" height="80" src={isModalActive ? closeMenu : openMenu} alt="" />
+          onClick={showModal}>
+          <img
+            width="80"
+            height="80"
+            src={isModalActive ? closeMenu : openMenu}
+            alt=""
+          />
         </button>
-
       </nav>
       <SuccessMsg />
     </header>
-  )
-}
-
+  );
+};
 
 export default Header;
