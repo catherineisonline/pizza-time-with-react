5cf642bb4d805c078d3c1c734a55adb7716e3a0a
catherineisonline <ekaterine.jpg@gmail.com>
2025-02-28 12:50:05 +0530

    refactor footer"

--- src/components/footer/FooterMenu.jsx
+++ src/components/footer/FooterMenu.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import FooterSocials from "./FooterSocials";
 import ResetLocation from "../../helpers/ResetLocation";
 const footerMenu = [
   {
@@ -22,22 +21,21 @@ const footerMenu = [
 ]
 const FooterMenu = () => {
   return (
-    <ul className="footer__menu  flex-container flex-column">
-      {footerMenu.map(({ to, label }) =>
-        <li key={to}>
-          <NavLink
-            className={({ isActive }) => `txt-white ${isActive ? "footer-active-link" : ""}`}
-            aria-current={({ isActive }) => (isActive ? "page" : undefined)}
-            onClick={ResetLocation}
-            to={to}>
-            {label}
-          </NavLink>
-        </li>
-      )}
-      <li>
-        <FooterSocials />
-      </li>
-    </ul>
+    <nav aria-label="Main Menu">
+      <ul className="footer__menu  flex-container flex-column">
+        {footerMenu.map(({ to, label }) =>
+          <li key={to}>
+            <NavLink
+              className={({ isActive }) => `txt-white ${isActive ? "footer-active-link" : ""}`}
+              aria-current={({ isActive }) => (isActive ? "page" : undefined)}
+              onClick={ResetLocation}
+              to={to}>
+              {label}
+            </NavLink>
+          </li>
+        )}
+      </ul>
+    </nav>
   );
 }
 export default FooterMenu
--- src/components/footer/FooterSocials.jsx
+++ src/components/footer/FooterSocials.jsx
@@ -8,19 +8,19 @@ const socials = [
     id: 1,
     href: "https://github.com/catherineisonline/pizza-time-with-react",
     img: Github,
-    name: Github
+    name: "GitHub"
   },
   {
     id: 2,
     href: "https://www.linkedin.com/in/catherinemitagvaria/",
     img: Linkedin,
-    name: Linkedin
+    name: "LinkedIn"
   },
   {
     id: 3,
     href: "https://ekaterine-mitagvaria.vercel.app/",
     img: Website,
-    name: Website
+    name: "Personal Portfolio"
   }
 ];
 
@@ -28,16 +28,15 @@ const FooterSocials = () => {
 
   return (
     <ul className="footer__socials">
-      {socials.map(social =>
-        <li key={social.id}>
+      {socials.map(({ id, href, img, name }) =>
+        <li key={id}>
           <a
-            href={social.href}
+            href={href}
             target="_blank"
             rel="noreferrer noopener"
-            aria-label={social.name}
+            aria-label={`Visit ${name}`}
           >
-            <span aria-hidden="true">{social.name}</span>
-            <img src={social.img} alt="" aria-hidden="true" width={50}
+            <img src={img} alt="" aria-hidden="true" width={50}
               height={50} />
           </a>
         </li>)}
