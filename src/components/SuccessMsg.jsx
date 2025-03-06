eb82e7a3dd40e497d3748e3d9ac07f14e2d27612
catherineisonline <ekaterine.jpg@gmail.com>
2025-03-06 22:57:43 +0530

    add accessibility to message

--- src/components/SuccessMsg.jsx
+++ src/components/SuccessMsg.jsx
@@ -2,9 +2,12 @@ import React from "react";
 
 const SuccessMsg = () => {
   return (
-    <section className="success">
+    <section
+      className="success"
+      role="alert"
+      aria-live="assertive">
       <p>item added to cart!</p>
     </section>
   );
-}
-export default SuccessMsg
+};
+export default SuccessMsg;
