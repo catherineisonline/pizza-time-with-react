724143b43d73dbb6d4ec9d5c8dd694c5ad1cc0fb
catherineisonline <ekaterine.jpg@gmail.com>
2025-03-10 22:50:23 +0530

    refactor blog post

--- src/routes/blog-post/BlogPost.jsx
+++ src/routes/blog-post/BlogPost.jsx
@@ -1,31 +1,35 @@
+import "./blog-post.css";
 import { useEffect, useState } from "react";
 import blogPosts from "../../data/blog-posts";
-import "./blog-post.css";
+import { useParams } from "react-router-dom";
+
 const BlogPost = () => {
+  const { name } = useParams();
   const [blogPost, setBlogPost] = useState({});
+
+  useEffect(() => {
+    setBlogPost(
+      blogPosts.find(
+        (post) => post.name.toLowerCase() === name.replaceAll("-", " ")
+      ) || {}
+    );
+  }, [name]);
+
   useEffect(() => {
     document.title = `${blogPost.name} | Pizza Time`;
   }, [blogPost.name]);
-  useEffect(() => {
-    setBlogPost(
-      blogPosts.filter(
-        (post) =>
-          post.name.toLowerCase() ===
-          window.location.pathname.toString().substring(6).replaceAll("-", " ")
-      )[0]
-    );
-  }, []);
   return (
     <main className="blog-post">
-      <img
-        src={blogPost.img}
-        alt=""
-        aria-hidden="true"
-      />
-      <section>
+      <article>
+        <img
+          src={blogPost.img}
+          alt=""
+          aria-hidden="true"
+          loading="lazy"
+        />
         <h2>{blogPost.name}</h2>
         <p>{blogPost.content}</p>
-      </section>
+      </article>
     </main>
   );
 };
