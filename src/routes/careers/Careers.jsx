108652b0a83560c7148d4e21032c73474b2347de
catherineisonline <ekaterine.jpg@gmail.com>
2025-03-12 19:27:28 +0530

    add semantics to career

--- src/routes/careers/Careers.jsx
+++ src/routes/careers/Careers.jsx
@@ -1,33 +1,41 @@
+import "./careers.css";
 import { Link } from "react-router-dom";
 import { useEffect } from "react";
-//components
 import ResetLocation from "../../helpers/ResetLocation";
-//data
 import careers from "../../data/careers";
-import './careers.css'
 
 const Careers = () => {
-    useEffect(() => {
-        document.title = "Careers | Pizza Time";
-    }, []);
-    return (
-        <main className="careers">
-            <h2>Careers</h2>
-            <h3>If you think you can add value with your expertise, passion, and hard work, you might be the ONE!</h3>
-            <section className="careers__inner">
-                {careers.map((career) =>
-                    <section key={career.id} className="careers__listings">
-                        <section>
-                            <h4>{career.title}</h4>
-                            <p>{career.description}</p>
-                            <hr aria-hidden="true" />
-                        </section>
-                        <Link to="/contact" onClick={ResetLocation}>Apply</Link>
-                    </section>
-                )}
-            </section>
-        </main>
-    )
-}
+  useEffect(() => {
+    document.title = "Careers | Pizza Time";
+  }, []);
+  return (
+    <main className="careers">
+      <h2>Careers</h2>
+      <h3>
+        If you think you can add value with your expertise, passion, and hard
+        work, you might be the ONE!
+      </h3>
+      <ul className="careers__inner">
+        {careers.map((career) => (
+          <li
+            key={career.id}
+            className="careers__listings">
+            <div>
+              <h4>{career.title}</h4>
+              <p>{career.description}</p>
+              <hr aria-hidden="true" />
+            </div>
+            <Link
+              to="/contact"
+              aria-label={`Apply for ${career.title}`}
+              onClick={ResetLocation}>
+              Apply
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+};
 
 export default Careers;
