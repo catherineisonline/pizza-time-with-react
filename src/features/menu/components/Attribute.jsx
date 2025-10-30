3632d88639e06182fb641a817902c5b56c097650
catherineisonline <ekaterine.jpg@gmail.com>
2025-10-30 15:54:34 +0700

    improve keyboard accessibility and add better focus styles

--- src/features/menu/components/Attribute.jsx
+++ src/features/menu/components/Attribute.jsx
@@ -1,28 +1,26 @@
-const Attribute = ({
-  attribute,
-  handleSelectedAttributes,
-  className,
-  targetAttribute,
-}) => {
+const Attribute = ({ attribute, handleSelectedAttributes, className, targetAttribute }) => {
   return (
     <ul className={className}>
       {attribute.attributeOptions?.map((item) => {
         return (
-          <li
-            key={item.id}
-            onClick={() => {
-              handleSelectedAttributes(attribute.id, item.value);
-            }}
-            style={
-              targetAttribute === item.value
-                ? {
-                    transition: "all 0.3s ease-in-out",
-                    color: "white",
-                    borderColor: "#fac564",
-                  }
-                : undefined
-            }>
-            {item.value}
+          <li key={item.id}>
+            <button
+              type="button"
+              onClick={() => {
+                handleSelectedAttributes(attribute.id, item.value);
+              }}
+              style={
+                targetAttribute === item.value
+                  ? {
+                      transition: "all 0.3s ease-in-out",
+                      color: "white",
+                      borderColor: "#fac564",
+                    }
+                  : undefined
+              }>
+              {" "}
+              {item.value}
+            </button>
           </li>
         );
       })}
